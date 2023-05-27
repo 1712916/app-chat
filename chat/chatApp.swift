7d94712de29d1382afb3bfabc9a611f14593aed2
1712916 <kieuphong2840@gmail.com>
2023-05-27 13:30:40 +0700

    create project

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Vinh Ngo on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
