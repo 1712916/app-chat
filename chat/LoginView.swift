2fe8b42fb93cd651b3afd324b509b572541b0f81
1712916 <kieuphong2840@gmail.com>
2023-06-28 21:53:38 +0700

    select image by image picker

--- chat/LoginView.swift
+++ chat/LoginView.swift
@@ -27,51 +27,72 @@ struct LoginView: View {
     @State var email = ""
     @State var password = ""
     @State var loginStatusMessage = ""
+    @State var shouldShowImagePicker = false
 
     var body: some View {
-        ScrollView {
-            VStack {
-                Picker(selection: $isLoginMode, label: Text("Picker Here")) {
-                    Text("Login").tag(true)
-                    Text("Create Account").tag(false)
-                }.pickerStyle(SegmentedPickerStyle())
-                
-                if isLoginMode {
-                    Button {} label: {
-                        Image(systemName: "person.fill").font(.system(size: 64))
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Picker(selection: $isLoginMode, label: Text("Picker Here")) {
+                        Text("Login").tag(true)
+                        Text("Create Account").tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode {
+                        Button {
+                            shouldShowImagePicker.toggle()
+                        } label: {
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 128, height: 128)
+                                        .cornerRadius(64)
+                                        .scaledToFill()
+                                } else {
+                                    Image(systemName: "person.fill").font(.system(size: 64)).padding().foregroundColor(Color(.label))
+                                }
+                            }.overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.black, lineWidth: 3))
+                            
+                        }
                     }
-                }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.none)
+                         
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(.white)
                 
-                Group {
-                    TextField("Email", text: $email)
-                        .keyboardType(.emailAddress)
-                        .textInputAutocapitalization(.none)
-                     
-                    SecureField("Password", text: $password)
-                }
-                .padding(12)
-                .background(.white)
-            
-                Button {
-                    handleAction()
-                } label: {
-                    HStack {
-                        Spacer()
-                        Text(isLoginMode ? "Login" : "Create Account")
-                            .foregroundColor(.white)
-                            .padding(.vertical, 10)
-                            .font(.system(size: 14, weight: .semibold))
-                        Spacer()
-                    }.background(Color.blue)
-                }
-                
-                Text(self.loginStatusMessage)
-                    .foregroundColor(.red)
-                
-            }.padding()
-        }.navigationTitle(isLoginMode ? "Login" : "Create Account")
-            .background(Color(.init(white: 0, alpha: 0.05)).ignoresSafeArea())
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Login" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
+                    
+                }.padding()
+            }.navigationTitle(isLoginMode ? "Login" : "Create Account")
+                .background(Color(.init(white: 0, alpha: 0.05)).ignoresSafeArea())
+        }.navigationViewStyle(StackNavigationViewStyle())
+            .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+                ImagePicker(image: $image)
+            }
     }
+    
+    @State var image: UIImage?
     
     private func handleAction() {
         if isLoginMode {
@@ -116,4 +137,44 @@ struct SignInSignUp_Previews: PreviewProvider {
             LoginView()
         }
     }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    
+    private let controller = UIImagePickerController()
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            parent.image = info[.originalImage] as? UIImage
+            picker.dismiss(animated: true)
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+        
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
 }
